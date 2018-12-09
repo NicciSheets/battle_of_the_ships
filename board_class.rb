39197fa50f20be4d634539083257d53258cfc408
Nicci Sheets <niccisheets@aol.com>
2018-12-09 03:33:44 -0500

    got correct grid size to correlate to each difficulty level

--- board_class.rb
+++ board_class.rb
@@ -2,12 +2,19 @@ class Board
 
 	attr_reader :difficulty
 
+DIFF_LEVEL = {
+		"beginner"     => 12, 
+		"intermediate" => 24, 
+		"advanced"     => 36
+	}
 
 
 def initialize(difficulty)
 	@difficulty = difficulty	
 end
 
-
+def grid_size?
+	DIFF_LEVEL[difficulty]
+end
 
 end
--- tdd_board_class.rb
+++ tdd_board_class.rb
@@ -8,4 +8,13 @@ class TddBoardClass < Minitest::Test
 		assert_equal(Board, board1.class)
 		assert_equal("beginner", board1.difficulty)
 	end
+
+	def test_correct_grid_size_returns
+		board1 = Board.new("beginner")
+		assert_equal(12, board1.grid_size?)
+		board2 = Board.new("intermediate")
+		assert_equal(24, board2.grid_size?)
+		board3 = Board.new("advanced")
+		assert_equal(36, board3.grid_size?)
+	end
 end
