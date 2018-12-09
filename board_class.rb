fe756ac16bcfd07b7252baae5554d60c4f57a384
Nicci Sheets <niccisheets@aol.com>
2018-12-09 05:24:02 -0500

    refactored a little on the board class

--- board_class.rb
+++ board_class.rb
@@ -1,30 +1,33 @@
 class Board
 
-	DIFF_LEVEL_SIZE = {
+	GRID_SIZE = {
 			"beginner"     => 12, 
 			"intermediate" => 24, 
 			"advanced"     => 36
 		}
 
-	attr_reader :difficulty
+	attr_reader :difficulty, :grid_size
 
 
-# initializes Board class with the difficulty that user will eventually be able to choose (will be one of the keys from the constant DIFF_LEVEL)
+# initializes Board class with the difficulty that user will eventually be able to choose, as well as its corresponding length for the grid size
 	def initialize(difficulty)
-		@difficulty = difficulty	
+		@difficulty = difficulty
+		@grid_size = GRID_SIZE[difficulty]	
 	end
 
 
-# this gives you the grid size that is associated with the diff level that is chosen
-	def grid_size?
-		DIFF_LEVEL_SIZE[difficulty]
-	end
-
+# this lays out the grid according to the size for each difficulty level, as an array of arrays
+# useful for seeing what grid looks like in terminal
 	def grid
 		val = nil
-		x = Array.new(self.grid_size?) { Array.new(self.grid_size?) { val } }	
+		x = Array.new(self.grid_size) { Array.new(self.grid_size) { val } }	
 		x.each do |row|
-			row
-		end	
+			p row
+		end
 	end
+
 end
+
+# board1 = Board.new("intermediate")
+# board1.grid
+# p board1
--- tdd_board_class.rb
+++ tdd_board_class.rb
@@ -7,20 +7,13 @@ class TddBoardClass < Minitest::Test
 		board1 = Board.new("beginner")
 		assert_equal(Board, board1.class)
 		assert_equal("beginner", board1.difficulty)
-	end
-
-	def test_correct_grid_size_returns
-		board1 = Board.new("beginner")
-		assert_equal(12, board1.grid_size?)
-		board2 = Board.new("intermediate")
-		assert_equal(24, board2.grid_size?)
-		board3 = Board.new("advanced")
-		assert_equal(36, board3.grid_size?)
+		assert_equal(12, board1.grid_size)
 	end
 
 	def test_grid
 		board1 = Board.new("beginner")
 		beg_grid = board1.grid
+		# p beg_grid
 		# this is an array of 12 arrays that have 12 indexes in each array
 		assert_equal(12, beg_grid.length)
 		# this shows that there's individual arrays that hold 12 items each within the larger "grid" array
