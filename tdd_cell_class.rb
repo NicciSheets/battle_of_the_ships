312c88bd56027fd062d3fe7e93e4874536db9259
Nicci Sheets <niccisheets@aol.com>
2019-01-13 05:11:49 -0500

    tweaked the to_s method

--- tdd_cell_class.rb
+++ tdd_cell_class.rb
@@ -4,22 +4,23 @@ require_relative "cell_class.rb"
 class TddCellClass < Minitest::Test
 
 	def test_cell_is_class
-		cell = Cell.new(:B4)
+		cell = Cell.new("B4")
 		# p cell
 		assert_equal(Cell, cell.class)
-		assert_equal(:B4, cell.coordinates)
+		assert_equal("B4", cell.coordinates)
 		assert_equal(:empty, cell.status)
 	end
 
 	def test_status_is_hit_or_miss
-		cell = Cell.new(:B4)
+		cell = Cell.new("B4")
 		assert_equal(:hit, cell.hit_ship)
 		assert_equal(:miss, cell.miss_ship)
 	end
 
 	def test_cell_class_returns_as_cell_status_with_to_s
-		cell = Cell.new(:B4)
+		cell = Cell.new("B4")
 		assert_equal("hit", cell.hit_ship.to_s)
+		assert_equal("B4", cell.coordinates)
 		assert_equal("miss", cell.miss_ship.to_s)
 	end
 end
