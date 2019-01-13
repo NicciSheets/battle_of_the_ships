3b35ad18e648170ed2eb194f2269832c8819bb1f
Nicci Sheets <niccisheets@aol.com>
2019-01-13 05:08:19 -0500

    working on cell class for coordinates and determining status of each cell

--- cell_class.rb
+++ cell_class.rb
@@ -2,9 +2,35 @@ require_relative "ship_class.rb"
 
 class Cell
 
+	Status = {
+		:empty      => ".", 
+		:hit        => "X",
+		:miss       => "0", 
+		:battleship => "B",
+		:cruiser    => "C",
+		:submarine  => "S",
+		:destroyer  => "D"
+	}
+
+# intiliazes Cell class with @coordinates for each individual cell
 	def initialize(coordinates)
 		@coordinates = coordinates
+		@status = :empty
 	end
 
-	attr_reader :coordinates
+	attr_reader :coordinates, :status
+
+	def hit_ship
+		@status = :hit
+	end
+
+	def miss_ship
+		@status = :miss
+	end
+
+	def to_s
+		Status[@status]
+	end
+
+
 end
