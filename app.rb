183653d9a3825fe05ac61c08bb35183d88d3a3ba
Nicci Sheets <niccisheets@aol.com>
2019-02-01 23:54:25 -0500

    got each board to show to begin play

--- app.rb
+++ app.rb
@@ -2,35 +2,40 @@ require 'sinatra'
 require_relative 'board_class.rb'
 require_relative 'player_class.rb'
 require_relative 'board_class.rb'
-# require_relative 'game_class.rb'
+# require_relative 'game_class_app.rb'
 enable :sessions
 
 get '/' do
-	
   erb :start_page
 end
 
 post '/start_page' do
 	p "params in start page are #{params}"
 	difficulty = params[:difficulty]
-	player1 = params[:player1]
-	redirect '/game_play?difficulty=' + difficulty + '&player1=' + player1
+	player = params[:player]
+
+	redirect '/game_play?difficulty=' + difficulty + '&player=' + player
 end
 
 get '/game_play' do
 	difficulty = params[:difficulty].to_sym
-	player1 = params[:player1]
-	@player1 = Player.new(player1, difficulty)
+	player = params[:player]
+
+	# game = Game.new
+
+	@player1 = Player.new(player, difficulty)
 	@opponent = Player.new("Enemy", difficulty)
-	erb :game_play, locals: {difficulty: difficulty, player1: player1}
+
+	erb :game_play, locals: {difficulty: difficulty, player: player, player1: @player1, opponent: @opponent}
 end
 
 post '/game_play' do
 	p "params in game play are #{params}"
 	difficulty = params[:difficulty]
-	player1 = params[:player1]
-	# @player1 = Player.new(player1, difficulty)
-	# @opponent = Player.new("Enemy", difficulty)
-	
-	redirect '/game_play?player1=' + player1 + '&difficulty=' + difficulty
+	# player1 = params[:player1]
+	# opponent = params[:opponent]
+	@player1 = @player1 || ""
+	@opponent = @opponent || ""
+
+	redirect '/game_play?player1=' + @player1 + '&difficulty=' + difficulty + '&opponent=' + @opponent
 end
